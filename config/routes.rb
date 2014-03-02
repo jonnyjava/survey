6701eab2dc88b41ac0ae487d692e5a31fdc5e68a
jonnyjava <jonnyjava.net@gmail.com>
2014-03-03 00:17:45 +0100

    nesting routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Survey::Engine.routes.draw do
 
   root to: "polls#index"
   resources :polls do
-    resources :questions do
+    resources :questions, :boolean_questions, :single_choice_questions, :multiple_choice_questions do
 			resources :question_options
 		end
   end
